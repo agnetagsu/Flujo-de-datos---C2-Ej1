0320d96de1e21dff846c02c44017b1be2844d7e4
Agneta <agnetagsu@yahoo.es>
2021-05-22 19:41:32 +0200

    Agregar archivos de proyecto.

---
+++ Flujo de datos - C2 Ej1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Flujo_de_datos___C2_Ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            int C = Convert.ToInt32(Console.ReadLine());
+            int D = A + B + C;
+            Console.WriteLine("La variable D es {0}+{1}+{2} = {3}",A,B,C,D);
+        }
+    }
+}
